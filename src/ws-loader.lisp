d7f0384e31c9748d057a3fda86be733f22e3e073
Imre Tabur <imre.tabur@eesti.ee>
2022-12-04 12:13:28 +0200

    Working web server

--- src/ws-loader.lisp
+++ src/ws-loader.lisp
@@ -1,8 +1,11 @@
 ;;;; quicklisp and ASDF loader and execution file.
+;(required "asdf")
+;(load "quicklisp")
+(asdf:already-loaded-systems)
 
 (pushnew (truename "../") ql:*local-project-directories* )
 (ql:register-local-projects)
 (ql:quickload :cl-ws-start-project)
 
 (cl-ws-start-project/ws-main:main-fun)
-(quit)
+;(quit)
--- src/ws-main.lisp
+++ src/ws-main.lisp
@@ -11,4 +11,12 @@
 (in-package :cl-ws-start-project/ws-main)
 
 (defun main-fun ()
-  (format t "Starting CL WS~%"))
+  (format t "Starting CL WS~%")
+  (defvar *acceptor*)
+  (setq *acceptor* (make-instance 'hunchentoot:easy-acceptor :port 4242))
+
+  (hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
+    (setf (hunchentoot:content-type*) "text/plain")
+    (format nil "Hey~@[ ~A~]!" name))
+
+  (hunchentoot:start *acceptor*))
